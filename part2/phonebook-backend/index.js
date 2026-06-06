e4e91a987edfbd4a367f82d75b85cd4972e3982f
samsmithsmithsam992-lgtm <samsmithsmithsam992@gmail.com>
2026-06-06 12:08:23 +0530

    Serve frontend from backend

--- part2/phonebook-backend/index.js
+++ part2/phonebook-backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
+const path = require('path')
 const app = express()
+
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static('dist'))
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
 })
